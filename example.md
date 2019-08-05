114c820e5c0412ff3701ace77de0da137ea136e2
Francois Gindraud <francois.gindraud@univ-lyon1.fr>
2019-08-05 17:43:15 +0200

    Start parser

---
+++ example.md
@@ -0,0 +1,19 @@
+# Général #
+
+*Wimd* est un outil pour générer un *wiki* depuis un fichier *markdown* contenant des notes.
+Wimd est écrit en *Rust*.
+
+## License ##
+Wimd n'a pas encore de license ; cela viendra plus tard.
+
+## Tests de formattage ##
+Un mot clé en **gras**.
+
+- premier élément
+- deuxième élément
+- troisième élément,
+sur plusieurs lignes
+
+Une liste avec entête :
+1. ligne 1
+2. ligne 2
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ fn read_stdin() -> Result<String, String> {
 }
 
 fn main() -> Result<(), String> {
-    let markdown = read_stdin()?;
-    for event in pulldown_cmark::Parser::new(&markdown) {
-        println!("{:?}", event)
-    }
+    let text = read_stdin()?;
+    markdown::print_event_list(&text);
+    let first_pass = markdown::first_pass(&text)?;
+    println!("FIRST {:?}", first_pass.strings);
     Ok(())
 }
 
@@ -23,8 +23,9 @@ fn main() -> Result<(), String> {
 // - keyword : extract list of keywords
 
 // Keywords:
-// - emphasized words added as kwds once
+// - emphasized words added as kwds once.
 // - links generated for all occurences.
+// Do not use links "[<kwd>]" as handling is more complex.
 
 // Description / Associate many things with keywords:
 // Sentence version "<kwd> : text ; text ; text."
@@ -36,45 +37,92 @@ fn main() -> Result<(), String> {
 // - show data in order of .md file (headings, etc). links for keywords
 // - by keyword, list of sentences organised by heading position
 
-mod database {
-    use std::collections::HashMap;
-    use std::ops;
+mod markdown {
+    use pulldown_cmark::{Event, Parser};
 
-    struct OriginPosition {
-        lines: ops::Range<usize>,
-        file: usize,
-    }
-    struct Sentence {
-        sentence: String,
-        heading: HeadingPosition,
-        origin: OriginPosition,
-    }
-    struct Heading {
-        text: String,
-        sub_headings: Vec<Heading>,
-        sentences: ops::Range<usize>,
-        origin: OriginPosition,
-    }
-    struct HeadingPosition {
-        file_index: usize,
-        heading_indexes: [Option<usize>; 6],
-    }
-    struct File {
-        filename: String,
-        headings: Vec<Heading>,
-    }
-    struct Keyword {
-        keyword: String,
-        in_sentences: Vec<usize>,
+    pub fn print_event_list(text: &str) {
+        for event in Parser::new(&text) {
+            println!("{:?}", event)
+        }
     }
 
-    struct Database {
-        /// All sentences (not heading)
-        sentences: Vec<Sentence>,
-        /// File & heading tree
-        files: Vec<File>,
-        /// All found keywords
-        keywords: Vec<Keyword>,
-        keyword_indexes: HashMap<String, usize>,
+    // Single markdown file
+    struct MarkdownDocument {
+        /// All text segments, in order of appearance.
+        strings: Vec<String>,
+        /// All keywords.
+        keywords: Vec<String>,
     }
+
+    pub struct FirstPassOutput {
+        pub strings: Vec<String>,
+        keywords: Vec<String>,
+    }
+
+    // First pass : extract sentences, structure, keywords
+    pub fn first_pass(text: &str) -> Result<FirstPassOutput, String> {
+        let mut strings = Vec::new();
+        let mut keywords = Vec::new();
+
+        // Use peekable iterator to avoid accumulator ?
+        // Filter accumulated strings ?
+
+        let mut current_string = String::new();
+        for event in Parser::new(text) {
+            match event {
+                // Text elements can be split if they contain markers, regroup them
+                Event::Text(text) => current_string.push_str(&text),
+                Event::Start(_) | Event::End(_) => {
+                    let s = std::mem::replace(&mut current_string, String::new());
+                    strings.extend(s.split('.').map(|s| s.to_string()))
+                }
+                // Ignore breaks
+                Event::SoftBreak => (),
+                Event::HardBreak => (),
+                // Everything else is rejected for now
+                _ => return Err(format!("Event not supported: {:?}", event)),
+            }
+        }
+
+        Ok(FirstPassOutput { strings, keywords })
+    }
+
 }
+
+/*struct OriginPosition {
+    lines: ops::Range<usize>,
+    file: usize,
+}
+struct Sentence {
+    sentence: String,
+    heading: HeadingPosition,
+    origin: OriginPosition,
+}
+struct Heading {
+    text: String,
+    sub_headings: Vec<Heading>,
+    sentences: ops::Range<usize>,
+    origin: OriginPosition,
+}
+struct HeadingPosition {
+    file_index: usize,
+    heading_indexes: [Option<usize>; 6],
+}
+struct File {
+    filename: String,
+    headings: Vec<Heading>,
+}
+struct Keyword {
+    keyword: String,
+    in_sentences: Vec<usize>,
+}
+
+struct Database {
+    /// All sentences (not heading)
+    sentences: Vec<Sentence>,
+    /// File & heading tree
+    files: Vec<File>,
+    /// All found keywords
+    keywords: Vec<Keyword>,
+    keyword_indexes: HashMap<String, usize>,
+}*/
